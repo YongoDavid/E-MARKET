d3567994bbce0de5d03414050e679e50fc4bd1d5
Yongo David <davidyongo9@gmail.com>
2024-01-25 23:10:08 +0100

    Data scripts uploaded

---
+++ Client/Data/allproducts.js
@@ -0,0 +1,105 @@
+const allProducts = [
+    {
+        id:1,
+        Name: 'HAVIT HV-G92 Gamepad',
+        Price: '$120',
+        Discount: '$160'
+    },
+
+    {
+        id:2,
+        Name: 'AK-900 Wired Keyboard',
+        Price: '$960',
+        Discount: '$1160'
+    },
+
+    {
+        id:3,
+        Name: 'IPS LCD Gaming Monitor',
+        Price: '$370',
+        Discount: '$400'
+    },
+
+    {
+        id:4,
+        Name: 'S-Series Comfort Chair',
+        Price: '$375',
+        Discount: '$400'
+    },
+
+    {
+        id:5,
+        Name: 'The north coat',
+        Price: '$260',
+        Discount: '$360'
+    },
+
+    {
+        id:6,
+        Name: 'Gucci duffle bag',
+        Price: '$960',
+        Discount: '$1160'
+    },
+
+    {
+        id:7,
+        Name: 'RGB liquid CPU Cooler',
+        Price: '$160',
+        Discount: '$170'
+    },
+
+    {
+        id:8,
+        Name: 'Small BookSelf',
+        Price: '$360'
+        
+    },
+
+    {
+        id:9,
+        Name: 'Breed Dry Dog Food',
+        Price: '$100'
+    },
+
+    {
+        id:10,
+        Name: 'CANON EOS DSLR',
+        Price: '$360'
+    },
+
+    {
+        id:11,
+        Name: 'ASUS Gaming Laptop',
+        Price: '$700'
+    },
+
+    {
+        id:12,
+        Name: 'Curology Product Set',
+        Price: '$500'
+    },
+
+    {
+        id:13,
+        Name: 'Kids Electric Car',
+        Price: '$960'
+    },
+
+    {
+        id:14,
+        Name: 'Jr. Zoom Soccer Cleats',
+        Price: '$1160'
+    },
+
+    {
+        id:15,
+        Name: 'GP11 USB Gamepad',
+        Price: '$660'
+    },
+
+    {
+        id:16,
+        Name: 'Quilted Satin Jacket',
+        Price: '$660'
+    }
+]
