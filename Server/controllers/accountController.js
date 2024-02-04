ca7ba85f327617574cf940253b89e85a7a94ada7
Yongo David <davidyongo9@gmail.com>
2024-02-04 22:09:53 +0100

    some update

--- Server/controllers/accountController.js
+++ Server/controllers/accountController.js
@@ -1,11 +1,11 @@
 // replace with account model here 
-const Accounts = require('../model/accountsModel')
-const jwt = require('jsonwebtoken')
+const Accounts = require('../model/accountsModel');
+const jwt = require('jsonwebtoken');
 
 // creating token function 
 const createToken = (id) => {
     return jwt.sign({id} , process.env.TOKEN_SECRET , {expiresIn:"24h"})
-}
+};
 
 // handles user signup function 
 
@@ -15,11 +15,11 @@ exports.signUp = async (req ,res) => {
     try{
         const user = await Accounts.signUp(name , email , password)
         res.status(200).json({ message: "Account Created"})
-        console.log(user)q
+        console.log(user)
     } catch (error) {
         res.status(400).json({error: error.message})
-    }
-}
+    };
+};
 
 // handles user login function 
 
@@ -32,6 +32,6 @@ exports.login = async (req,res) => {
         res.status(200).json({user , token , message:'user logged in '})
     } catch (error){
         res.status(400).json({error:error.message})
-    }
-}
+    };
+};
 
