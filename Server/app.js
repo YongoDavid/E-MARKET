daea728353dae21f5916ffee93cf8f8a7fe38d8d
Yongo David <davidyongo9@gmail.com>
2024-02-04 22:13:06 +0100

    app.js update

--- Server/app.js
+++ Server/app.js
@@ -1,42 +1,42 @@
 // dependencies installed // cors bcrypt express dotenv mysql2 validator morgan  jsonwebtoken uuid ws
 
 // STILL HAVENT RESOLVED THE ISSUE THAT MIGHT BE COMING FROM HERE 
-const path = require('path')
-const express = require('express') // import express package 
-const http = require('http') // too create the server 
-const morgan = require('morgan')
-require('dotenv').config() // import the dot env file 
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const WebSocket = require('ws')
+const path = require('path');
+const express = require('express');// import express package 
+const http = require('http'); // too create the server 
+const morgan = require('morgan');
+require('dotenv').config(); // import the dot env file 
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const WebSocket = require('ws');
 
 // HERE IMPORT DIFFERENT ROUTES FOR ACCOUNT / USERS  
-const accountRoutes = require('./Routes/accountRoutes')
-const userRoutes = require('./Routes/userRoutes')
+const accountRoutes = require('./Routes/accountRoutes');
+const userRoutes = require('./Routes/userRoutes');
 
 // The code imports route handlers for accounts and users.
 
 // CREATING SERVER 
 
-const app = express()
-const PORT = 5500
+const app = express();
+const PORT = 5500;
 
-const db = require('./config/db')
+const db = require('./config/db');
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '..' , 'client')));
-app.use('/images' , express.static(path.join(__dirname, '..', 'client', 'images')))
+app.use('/images' , express.static(path.join(__dirname, '..', 'client', 'images')));
 // Middlewares 
-app.use(morgan('dev'))
-app.use(bodyParser.json())  // use to identify the body of the request  
+app.use(morgan('dev'));
+app.use(bodyParser.json());  // use to identify the body of the request  
 
 app.use(
     cors({
         // replace this when website is hosted  "https://the-market-uchx.onrender.com"
         origin: ["http://localhost:5500"],
         credentials: true,
-    })
-)
+    });
+);
 
 // using route handlers 
 app.use('/accounts' , (req , res , next) => {
@@ -44,11 +44,11 @@ app.use('/accounts' , (req , res , next) => {
     next();
 } , accountRoutes);
 // app.use('/accounts', accountRoutes )
-app.use('/user', userRoutes)
+app.use('/user', userRoutes);
 
 // handlers are applied to specific paths in the Express app.
 
-const server = http.createServer(app)  // to create out http server and pass our express server into it.
+const server = http.createServer(app);  // to create out http server and pass our express server into it.
 const wss = new WebSocket.Server({server}); // creating WebSocket server 
 
 // creating  a web socket server connecion 
@@ -69,4 +69,4 @@ wss.on('connection' ,(ws) => {
 
 server.listen(PORT ,() => {
     console.log(`listening on PORT ${PORT}`)
-})
+});
